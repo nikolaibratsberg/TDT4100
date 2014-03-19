776b585c29cf80a6739e82474cb56711484b6583
Nikolai Bratsberg <nikolai.bratsberg@gmail.com>
2014-03-19 18:33:52 +0100

    ...

--- src/patterns/observable/StockIndex.java
+++ src/patterns/observable/StockIndex.java
@@ -1,21 +1,26 @@
 package patterns.observable;
 
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 
 public class StockIndex implements StockListener{
 	
 	private String name;
-	private double index; 
+	private double index;
+	private List<Stock> stocks;
 	
 
 	public StockIndex(String name, Stock... stocks) {
 		this.name = name;
-
+		this.stocks = new ArrayList<Stock>(Arrays.asList(stocks));
+		
 		for (Stock s : stocks) {
 			s.addStockListener(this);
 			this.index += s.getPrice();
-		
-		}	
+		}
+
+
 	}
 	
 	public void stockPriceChanged(Stock stock, double oldPrice, double newPrice) {
@@ -24,23 +29,29 @@ public class StockIndex implements StockListener{
 	
 	
 	public void addStock(Stock stock) {
-		stock.addStockListener(this);
-		this.index += stock.getPrice();
+		if (!this.stocks.contains(stock)) {
+			this.stocks.add(stock);
+			stock.addStockListener(this);
+			this.index += stock.getPrice();
+		}
+
 	}
+
 	
 	public void removeStock(Stock stock) {
-		stock.removeStockListener(this);
-		this.index -= stock.getPrice();
-		
+		if (this.stocks.contains(stock)) {
+			stock.removeStockListener(this);
+			this.stocks.remove(stock);
+			this.index -= stock.getPrice();
+		}
 	}
 	
 	public double getIndex() {
 		return this.index;
 	}
 	
-	public String toString() {
-		return "The stock index " + name + " is " + index;
+	public String getName() {
+		return this.name;
 	}
-	
 	
 }
